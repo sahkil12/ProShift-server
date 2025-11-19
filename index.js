600a359639f8f78e5c0e17d75d7179e58698704b
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-11-19 11:18:43 +0600

    added rider active api for get all the active riders and impliment serch function with rider name and district and added deactivate ride with rider id and change status in inactive

--- index.js
+++ index.js
@@ -254,6 +254,35 @@ async function run() {
                 res.status(500).send({ message: "Failed to load pending riders", error });
             }
         });
+        // active rider
+        app.get("/riders/active", async (req, res) => {
+            const search = req.query.search || "";
+
+            const filter = {
+                status: "Active",
+                $or: [
+                    { name: { $regex: search, $options: "i" } },
+                    { district: { $regex: search, $options: "i" } }
+                ]
+            };
+
+            const riders = await ridersCollection.find(filter).toArray();
+            res.send(riders);
+        });
+        // deactivate rider
+        app.patch("/riders/deactivate/:id", async (req, res) => {
+            const id = req.params.id;
+            const result = await ridersCollection.updateOne(
+                { _id: new ObjectId(id) },
+                {
+                    $set:
+                    {
+                        status: "Inactive"
+                    }
+                }
+            );
+            res.send(result);
+        });
 
         // approve rider
         app.patch("/riders/approve/:id", async (req, res) => {
